ab255440c1990efdd3bfb82034f973b00674dff4
Jagr Krtanjek <jagr@Jagrs-Air.krtanjek.local>
2024-09-28 22:32:37 -0400

    added proper env

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Define your backend API base URL
-const BASE_URL = os.environ["URL"]; // Replace with your actual backend URL
+const BASE_URL = process.env.URL; // Replace with your actual backend URL
 
 // Example function to fetch events from the backend
 export const fetchEvents = async (start, end, type) => {
